6a3837f2da22c887886e066ecf9e03cf5c486e92
tikendraw <tikendraksahu1029@gmail.com>
2024-06-06 16:08:37 +0530

    fix: selector updated

--- groqon/element_selectors.py
+++ groqon/element_selectors.py
@@ -7,9 +7,7 @@ PROMPT_TEXTAREA_SELECTOR = r"#radix-\:r3\: > textarea"
 END_TEXT_SELECTOR = r"body > main > div > div.flex.flex-col.md\:flex-col.md\:relative.w-full.max-w-\[900px\].bg-background.z-10.gap-2.md\:gap-6 > div > button.block.lg\:hidden > div > div"
 QUERY_INPUT_SELECTOR = "#chat"
 CHAT_INPUT_SELECTOR = "#chat"
-
-QUERY_SUBMIT_SELECTOR = ".self-end"
-
+QUERY_SUBMIT_SELECTOR = "body > main > div > div.hidden.md\:block.w-full > div > div.order-2.md\:order-1 > form > div > button"
 PROFILE_BUTTON_SELECTOR = r"#radix-\:r3\:"
 ADVANCED_SETTING_SELECTOR = r"#radix-\:rf\:"
 SYSTEM_PROMPT_BUTTON = r"button.py-1\.5:nth-child(1)"
@@ -18,4 +16,4 @@ PROMPT_SAVE_BUTTON_SELECTOR = r"html.__variable_b1da2a.__variable_aaf875.dark bo
 COPY_BUTTON_SELECTOR = r"body > main > div > div.grow.w-full.overflow-auto.mt-2.flex.flex-col.md\:flex-col > div.flex.flex-col.\[word-break\:break-word\].mb-6 > div.flex.flex-col.font-normal.text-base.border-l.border-mutedaccent.px-5 > div.flex.flex-row.align-center.items-center.gap-4.pt-4 > div:nth-child(1) > svg"
 MODEL_NAME_SELECTOR = r"body > header > div.flex-1.flex.items-center.justify-center.md\:col-span-4.md\:col-start-2 > div > button > p"
 
-SIGNIN_BUTTON_SELECTOR = r"body > footer > div.grid.grid-cols-3.items-end.pb-2 > div.flex.flex-col.items-start.px-6 > button.inline-flex.items-center.whitespace-nowrap.rounded-md.text-sm.font-medium.ring-offset-background.transition-colors.focus-visible\:outline-none.disabled\:pointer-events-none.disabled\:opacity-50.text-primary.underline-offset-4.h-10.justify-start.p-0.hover\:text-primaryaccent.hover\:no-underline"
+SIGNIN_BUTTON_SELECTOR = r"#radix-\:r6\:"

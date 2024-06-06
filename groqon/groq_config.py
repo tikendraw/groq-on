908f6ae4249c5ed16fc0fc387ac0f9e2fd840fe5
tikendraw <tikendraksahu1029@gmail.com>
2024-06-06 16:09:06 +0530

    fix: import fix from logger

--- groqon/groq_config.py
+++ groqon/groq_config.py
@@ -1,11 +1,6 @@
 import json
 import os
 from pathlib import Path
-
-from .logger import get_logger
-
-logger = get_logger(__name__)
-
 home_dir = os.path.expanduser("~")
 groq_config_folder = os.path.join(home_dir, ".groqon")
 GROQ_COOKIE_FILE = Path(groq_config_folder) / "groq_cookie.json"
@@ -15,7 +10,7 @@ MODEL_JSON_FILE = Path(groq_config_folder) / "models.json"
 URL = "https://groq.com/"
 API_URL = "https://api.groq.com/openai/v1/chat/completions"
 MODEL_URL = "https://api.groq.com/openai/v1/models"
-AUTHENTICATION_URL = "https://web.stytch.com/sdk/v1/sessions/authenticate"
+AUTHENTICATION_URL = 'https://web.stytch.com/sdk/v1/oauth/authenticate'
 
 GLOBAL_LIMITS = {
     "gemma-7b-it": {"requests_per_minute": 30, "calls": 18},
@@ -24,6 +19,9 @@ GLOBAL_LIMITS = {
     "mixtral-8x7b-32768": {"requests_per_minute": 30, "calls": 18},
 }
 
+# to avoid circular import error
+from .logger import get_logger # noqa
+logger = get_logger(__name__)
 
 def get_model_ids(models_file=MODEL_JSON_FILE):
     """

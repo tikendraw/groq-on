1db30c2fb89805c18c117e6db6bb6004b525bb5e
tikendraw <tikendraksahu1029@gmail.com>
2024-07-11 19:06:40 +0530

    removed useless vars

--- groqon/groq_config.py
+++ groqon/groq_config.py
@@ -1,57 +1,43 @@
-import json
 import os
 from pathlib import Path
+from .utils import save_config, get_model_ids
 
 home_dir = os.path.expanduser("~")
 groq_config_folder = os.path.join(home_dir, ".groqon")
+GROQON_CONFIG_FILE = Path(groq_config_folder) / "config.yaml"
 GROQ_COOKIE_FILE = Path(groq_config_folder) / "groq_cookie.json"
 MODEL_LIST_FILE = Path(groq_config_folder) / "models.json"
 
+TOP_P = 1
+STREAM = True
+TEMPERATURE = 0.1
+MAX_TOKENS = 2048
+SYSTEM_PROMPT = "Please try to provide useful, helpful and actionable answers."
 
 URL = "https://groq.com/"
 API_URL = "https://api.groq.com/openai/v1/chat/completions"
 MODEL_URL = "https://api.groq.com/openai/v1/models"
 AUTHENTICATION_URL = 'https://web.stytch.com/sdk/v1/oauth/authenticate'
+ENDTOKEN='<|endtoken|>'
+PORT = 8888
 
-GLOBAL_LIMITS = {
-    "gemma-7b-it": {"requests_per_minute": 30, "calls": 18},
-    "llama3-70b-8192": {"requests_per_minute": 30, "calls": 16},
-    "llama3-8b-8192": {"requests_per_minute": 30, "calls": 16},
-    "mixtral-8x7b-32768": {"requests_per_minute": 30, "calls": 18},
-}
-
-# to avoid circular import error
-from .logger import get_logger # noqa
-logger = get_logger(__name__)
-
-def get_model_ids(models_file=MODEL_LIST_FILE):
-    """
-    Reads the model IDs from the models.json file and populates the modelindex list.
-
-    Returns:
-        None
-    """
-    modelindex = []
-    try:
-
-        with open(models_file, "r") as f:
-            models_data = json.load(f)
-
-        modelindex.extend(
-            model["id"] for model in models_data["data"] if model.get("active")
-        )
-    except Exception as e:
-        logger.exception("Error reading models.json ", exc_info=e)
-        modelindex = [
-            "gemma-7b-it",
-            "llama3-70b-8192",
-            "llama3-8b-8192",
-            "mixtral-8x7b-32768",
-        ]
-
-    return modelindex
-
-
-modelindex = get_model_ids()
-
+modelindex = get_model_ids(models_file=MODEL_LIST_FILE)
 DEFAULT_MODEL = modelindex[2]  # Llama3-8b
+
+if not GROQON_CONFIG_FILE.exists():
+    config = {
+        'defaults': {
+            'cookie_file': str(GROQ_COOKIE_FILE.absolute()),
+            'models': modelindex,
+            'headless': True,
+            'n_workers': 2,
+            'reset_login': False,
+            'server_model_configs': str(MODEL_LIST_FILE.absolute()),
+            'verbose': False,
+            'print_output': True
+        }
+    }
+    
+    save_config(config, GROQON_CONFIG_FILE)
+    
+

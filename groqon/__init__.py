ed7d0bf53d8cf2e477f6ded389ab85c3400f0b87
tikendraw <tikendraksahu1029@gmail.com>
2024-07-11 23:59:48 +0530

    removed agroq

--- groqon/__init__.py
+++ groqon/__init__.py
@@ -1,10 +1,9 @@
 import os
 
-from .async_api.agroq import agroq
 from .groq_config import groq_config_folder
 from .logger import get_logger
 
 os.makedirs(groq_config_folder, exist_ok=True)
 
 
-__all__ = ["get_logger", 'agroq']
+__all__ = ["get_logger"]
